fd7966bb7032a227a22210d3a00f7844f2a49ba2
Eliza-Mira <liz142@hotmail.com>
2022-12-20 00:27:53 -0700

    Practice marker at quiz end of 3.2.7

--- Python_practice.py
+++ Python_practice.py
@@ -57,3 +57,66 @@ counties
 counties.append("Arapahoe")
 counties
 # testing comment
+
+counties_tuple = ("Arapahoe","Denver","Jefferson")
+len(counties_tuple)
+
+counties_tuple[1]
+
+counties_tuple[:-2]
+counties_tuple[:2]
+counties_tuple[:-1]
+counties_tuple[1:2]
+
+# create a dictionary
+#create an empty dictionary 1st
+counties_dict = {}
+
+counties_dict["Arapahoe"] = 422829
+counties_dict["Denver"] = 463353
+counties_dict["Jefferson"] = 432438
+
+counties_dict
+
+# To get only the keys
+counties_dict.keys()
+# retrieve only the values
+counties_dict.values()
+# Get a Specific Value
+counties_dict.get("Denver")
+
+# How would you get # of Arapahoe registered voters
+# ALL have the same output of 422829
+counties_dict['Arapahoe']
+counties_dict.get("Arapahoe")
+print(counties_dict['Arapahoe'])
+print(counties_dict.get("Arapahoe")) 
+# same quest but w/o quotes (single or double)
+counties_dict[Arapahoe]
+# INCLUDE the quotes
+counties_dict['Arapahoe']
+counties_dict["Arapahoe"]
+
+#create a list of dictionaries
+#Keys: country & registered voters
+
+# FIRST create empty list
+voting_data = []
+# add/append ea dict
+voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
+voting_data.append({"county":"Denver", "registered_voters": 463353})
+voting_data.append({"county":"Jefferson", "registered_voters": 432438})
+#test voting data dict
+voting_data
+
+#quiz Add El Paso & reg voters to 2nd position
+voting_data.append({"county": "El Paso", "registered_voters":461149})
+#test voting data dict
+voting_data
+
+#quiz Remove Arapanhoe & voters
+voting_data.remove({"county":"Arapahoe", "registered_voters": 422829})
+#test voting data dict
+voting_data
+
+#Keep Jefferson in 2nd but Move Denver & voters to 3rd
