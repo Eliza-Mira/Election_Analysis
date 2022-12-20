07e9abb67e33018767060849db28f1d5b84ad93b
Eliza-Mira <liz142@hotmail.com>
2022-12-19 20:49:08 -0700

    practice push

---
+++ Python_practice.py
@@ -0,0 +1,59 @@
+print("hello world")
+type(3)
+type(2,019)
+type("2,019")
+type(2019)
+ballots = 1,341
+ballots
+type(ballots)
+type(73.81)
+float(73.81)
+type("hello world")
+type(" ")
+type(True)
+type('True')
+type('True')
+print("Where am i?")
+
+counties = ["Arapahoe", "Denver", "Jefferson"]
+my_list=list()
+counties
+
+print(counties[2])
+print(counties[-1])
+print(counties[-2])
+
+len(counties)
+counties[0:2]
+
+counties[0:3]
+counties[0:1]
+len(counties)
+
+counties[1:]
+counties
+
+counties.append("El Paso")
+counties
+
+counties.insert(2, "El Paso")
+counties
+
+counties.pop(2)
+counties
+
+counties[2]="El Paso"
+counties
+
+counties.insert(1, "El Paso")
+counties
+
+counties.remove("Arapahoe")
+counties
+
+counties.insert(1, "Jefferson")
+counties
+
+counties.append("Arapahoe")
+counties
+# testing comment
